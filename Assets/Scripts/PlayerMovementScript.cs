c1b83d6fb020e6a1cffaa44bc76b093be7da9b2d
cteggatz <categgatzjr@gmail.com>
2023-12-10 00:21:47 -0600

    added basic player, ground, script & materials folder

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private BoxCollider2D _collider;
+
+    [SerializeField] private float hMoveSpeed = 200f;
+    [SerializeField] private float vMoveSpeed = 1000f;
+
+    private float hMoveDirection;
+    private bool canJump;
+
+
+    void Start(){
+        _rb = transform.GetComponent<Rigidbody2D>();
+        _collider = transform.GetComponent<BoxCollider2D>();
+
+        hMoveDirection = 0;
+        canJump = true;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        hMoveDirection = Input.GetAxisRaw("Horizontal") * hMoveSpeed;
+    }
+
+    void FixedUpdate(){
+        if(canJump){
+            _rb.AddForce(transform.right * hMoveDirection);
+
+            if(Input.GetAxisRaw("Vertical") > 0){
+                canJump = false;
+                _rb.AddForce(transform.up * vMoveSpeed);
+            } 
+        } else {
+            _rb.AddForce(transform.right * hMoveDirection/10);
+        }
+
+
+    }
+    void OnCollisionEnter2D(Collision2D other){
+        canJump = true;
+    }
+}
