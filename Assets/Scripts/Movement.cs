fd152ee4740bafc65ef714852e1b6268668109d0
WraithWren <mebarroso@madison.k12.wi.us>
2023-12-13 11:29:39 -0600

    Added new movement
    Changed player and ground

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     [SerializeField] private float DefaultSpeed, SprintSpeed, CurrentSpeed, SpeedLoss, SprintGain;
     [SerializeField] private float jumphight;
