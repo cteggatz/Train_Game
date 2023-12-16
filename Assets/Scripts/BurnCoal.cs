bba7fe33c7239c3917d7db2fda9ae7441145a141
WraithWren <mebarroso@madison.k12.wi.us>
2023-12-16 11:21:34 -0600

    Fuel UI

--- Assets/Scripts/BurnCoal.cs
+++ Assets/Scripts/BurnCoal.cs
@@ -1,25 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BurnCoal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float timer, fuel;
+    [SerializeField] private float delayAmount;
+    [SerializeField] private Text speedText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= delayAmount)
+        {
+            timer = 0f;
+            fuel -= 0.5f;
+        }
+        speedText.text = "Fuel: " + fuel.ToString();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Fuel")
         {
+            fuel += 10;
             Destroy(collision.gameObject);
         }
     }
