91cd164512a74af52fb99f7999b498ed6619a3e9
Martin E Meitner <memeitner@madison.k12.wi.us>
2024-01-19 12:04:52 -0600

    Updated Background Spawner
    now with (partially working) autosetting spawn positions

--- Assets/Scripts/Background/BackgroundSpawner.cs
+++ Assets/Scripts/Background/BackgroundSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,18 @@ using UnityEngine;
 public class BackgroundSpawner : MonoBehaviour
 {
     public GameObject backgroundPrefab;
-    [SerializeField] private float spawnPos = 40.1f;
+    [SerializeField] private float spawnPos = 40.01f;
 
     void Start(){
         spawnChunk();
+        spawnPos = transform.position.x + 0.01f + backgroundPrefab.GetComponent<SpriteRenderer>().sprite.bounds.extents.x;
+        Debug.Log(transform.position.x);
+        Debug.Log(backgroundPrefab.gameObject.transform.localScale.x);
     }
     private void spawnChunk(){
         //Debug.Log("spawned!");
         GameObject a = Instantiate(backgroundPrefab) as GameObject;
-        a.transform.position = new Vector2(spawnPos, -1);
+        a.transform.position = new Vector2(spawnPos,0.8f);
     }
     void OnTriggerExit2D(Collider2D bc)
     {
