9b5e435827b2c6d7c33000ef53b3a17f4ad457f7
Martin E Meitner <memeitner@madison.k12.wi.us>
2024-02-06 11:55:12 -0600

    Random changes
    not done

--- Assets/Scripts/Background/Backgroundinator.cs
+++ Assets/Scripts/Background/Backgroundinator.cs
@@ -8,18 +8,23 @@ public class BackgroundMover : MonoBehaviour
     private Rigidbody2D rb;
     private float deSpawn = -115f;
     public BoxCollider2D bc;
+    private GameObject Train;
     void Start()
     {
         bc = this.GetComponent<BoxCollider2D>();
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(-backSpeed, 0);
+        Train = GameObject.FindWithTag("Train (primary)");
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (transform.position.x < deSpawn){
+        Debug.Log(Train);
+        backSpeed = Train.GetComponent<Train_Controller>().GetSpeed();
+        if (transform.position.x < deSpawn){
             Destroy(this.gameObject);
-       }
+        }
     }
+    
 }
