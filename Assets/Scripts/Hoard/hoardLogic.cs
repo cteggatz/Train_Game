d627c52e94a3c30df2ff2fe2eb5d8c6618d1c62e
WraithWren <mebarroso@madison.k12.wi.us>
2024-02-27 21:08:09 -0600

    Fucking everything is broken
    FML. collission on the enemies is just broken now- I didn't even change shit with the collider

--- Assets/Scripts/Hoard/hoardLogic.cs
+++ Assets/Scripts/Hoard/hoardLogic.cs
@@ -6,10 +6,11 @@ using static Unity.Collections.Unicode;
 public class hoardLogic : MonoBehaviour
 {
     private float timer, gametime;
-    [SerializeField]  private float delayAmount, runtime;
+    [SerializeField]  private float delayAmount, runtime, runUpSpeed;
     [SerializeField, Range(0, 100)] private float difficulty;
     [SerializeField] private GameObject grunt, side_grunt;
     [SerializeField] private Vector2 H_location;
+    [SerializeField] private Vector2 spawnTarget;
     [SerializeField] private List<Transform> targets = new List<Transform>();
     
     // Update is called once per frame
@@ -28,8 +29,8 @@ public class hoardLogic : MonoBehaviour
         {
             timer = 0f;
             float random = Random.Range(0.5f, 1.5f);
-            if(random >= 0){
-                Instantiate(side_grunt).GetComponent<spawnOnTrain>().spawn = new Vector3(0.0f, 0.0f, 0.0f);
+            if(random <= 0){
+                spawnOnTrain();
             }
             else{
                 spawnGrunt(0, H_location, Random.Range(4, 6), (int) (10 * random));
@@ -51,6 +52,15 @@ public class hoardLogic : MonoBehaviour
         lad.GetComponent<Igiveup>().awareness = awareness;
         lad.GetComponent<Igiveup>().health = health;
         lad.transform.position = spawn;
+    }
+
+    private void spawnOnTrain()
+    {
+        float smooth = runUpSpeed * Time.deltaTime;
+        GameObject sideman = Instantiate(side_grunt);
+        sideman.transform.SetParent(gameObject.transform);
+        
+        spawnGrunt(0, spawnTarget, Random.Range(4, 6), (int)(10));
     }
 
 }
