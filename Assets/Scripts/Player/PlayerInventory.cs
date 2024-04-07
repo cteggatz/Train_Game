9004d0ee4530274d9f2729ebfffdfb899050e792
WraithWren <mebarroso@madison.k12.wi.us>
2024-04-07 17:10:42 -0500

    arms, basic train art, sprinting

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -34,7 +34,7 @@ public class PlayerInventory : MonoBehaviour
 
     //---- item rendering ----
     [Header("Item Renderer")]
-    [SerializeField] private GameObject itemRenderer;
+    [SerializeField] public GameObject itemRenderer;
     [SerializeField] private float itemDistance = 0.5f;
     [SerializeField] private Vector3 offset;
 
