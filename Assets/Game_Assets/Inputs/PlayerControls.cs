6cca1920c32903b30d48ced50dfb6b17590a5530
Martin E Meitner <memeitner@madison.k12.wi.us>
2024-04-11 11:36:21 -0500

    Revert "B E A N"
    This reverts commit 686b7aa52e3ef32f4f8b66e83bf899a9823dea6f.

--- Assets/Game_Assets/Inputs/PlayerControls.cs
+++ Assets/Game_Assets/Inputs/PlayerControls.cs
@@ -96,13 +96,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
             ]
         }
     ],
-    ""controlSchemes"": [
-        {
-            ""name"": ""Keyboard"",
-            ""bindingGroup"": ""Keyboard"",
-            ""devices"": []
-        }
-    ]
+    ""controlSchemes"": []
 }");
         // PlayerMovement(Keyboard)
         m_PlayerMovementKeyboard = asset.FindActionMap("PlayerMovement(Keyboard)", throwIfNotFound: true);
@@ -210,15 +204,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         }
     }
     public PlayerMovementKeyboardActions @PlayerMovementKeyboard => new PlayerMovementKeyboardActions(this);
-    private int m_KeyboardSchemeIndex = -1;
-    public InputControlScheme KeyboardScheme
-    {
-        get
-        {
-            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
-            return asset.controlSchemes[m_KeyboardSchemeIndex];
-        }
-    }
     public interface IPlayerMovementKeyboardActions
     {
         void OnMovement(InputAction.CallbackContext context);
